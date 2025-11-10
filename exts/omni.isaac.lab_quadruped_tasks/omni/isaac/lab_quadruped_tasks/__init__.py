5dec6409358c0f5ca6be676dafbde5ca77c0183f
carlo <awiezzz@qq.com>
2025-11-10 23:56:58 +0800

    feat:  rough terrain env for go2

--- exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/__init__.py
+++ exts/omni.isaac.lab_quadruped_tasks/omni/isaac/lab_quadruped_tasks/__init__.py
@@ -1,7 +1,60 @@
-def on_startup():
-    # 在扩展启动时注册你的机器人/任务（占位）
-    # 例如: register_quadruped_tasks()
-    pass
+"""
+License: MIT License
+Copyright (c) 2025, Felipe Mohr Santos
+"""
 
-def on_shutdown():
-    pass
+import gymnasium as gym
+
+from . import agents, go2_env_cfg
+
+##
+# Register Gym environments.
+##
+
+############################
+# Go2 Blind Flat Environment
+############################
+
+gym.register(
+    id="Isaac-Quadruped-Go2-Blind-Flat-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": go2_env_cfg.Go2BlindFlatEnvCfg,
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.Go2BlindFlatPPORunnerCfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Quadruped-Go2-Blind-Flat-Play-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": go2_env_cfg.Go2BlindFlatEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.Go2BlindFlatPPORunnerCfg,
+    },
+)
+
+#############################
+# Go2 Blind Rough Environment
+#############################
+
+gym.register(
+    id="Isaac-Quadruped-Go2-Blind-Rough-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": go2_env_cfg.Go2BlindRoughEnvCfg,
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.Go2BlindRoughPPORunnerCfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Quadruped-Go2-Blind-Rough-Play-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": go2_env_cfg.Go2BlindRoughEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.Go2BlindRoughPPORunnerCfg,
+    },
+)
